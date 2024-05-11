085616a45b6700da5faaace187fd75254be6154d
Yonnier-Rivas <yonnier.rivas@gmail.com>
2024-05-11 17:50:25 -0500

    Se crea la conexion par la nueva BD en firebase HypicarsV2

--- firebaseDB/config.js
+++ firebaseDB/config.js
@@ -1,13 +1,13 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyASkN4qYdrvELYE3NDEgfvnBeTYCoontMQ",
-    authDomain: "hypicars1.firebaseapp.com",
-    projectId: "hypicars1",
-    storageBucket: "hypicars1.appspot.com",
-    messagingSenderId: "85146214129",
-    appId: "1:85146214129:web:3ce2830ec728469d65d350",
-    measurementId: "G-S9H2MKJQ9V"
-  };
+  apiKey: "AIzaSyBUaMhCqr3FjSgwEPBjhWt6Aa_nli0aD5s",
+  authDomain: "hypicarsv2.firebaseapp.com",
+  projectId: "hypicarsv2",
+  storageBucket: "hypicarsv2.appspot.com",
+  messagingSenderId: "716505068134",
+  appId: "1:716505068134:web:c32c3b26f7c85ac07bb3a7",
+  measurementId: "G-TCC8PN0TN9"
+};
 
-  export default firebaseConfig;
+export default firebaseConfig;
